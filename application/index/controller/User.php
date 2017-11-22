63c350342b4da709fcebf4189021080474d17345
linjuming1981 <mingfish@qq.com>
2017-11-22 22:10:35 +0800

    ddd

--- application/index/controller/User.php
+++ application/index/controller/User.php
@@ -1,28 +1,64 @@
 <?php 
 namespace app\index\controller;
 use think\Controller;
-use think\model\User;
+// use think\model\User;
 use think\Session;
 
-class User extends controller{
+class User extends Controller{
 
+
+	// 登陆页
 	public function login(){
 		return $this->fetch();
 	}
 
+
+	// 提交登陆
 	public function doLogin(){
 
-		$username = input('post.uname');
+		$uname = input('post.uname');
 		$password = input('post.passworld');
-		$userM = new User;
-        $user = $userM->login($username, $password);
+		$userM = new \app\index\model\User;
+		$user = $userM->login($uname, $password);
 		if($user){
 			Session::set('uid', $user['uid']);
-            Session::set('uname', $user['uname'])
-            $this->success("登录成功"，'index/index/index');
+			Session::set('uname', $user['uname']);
+			$this->success("登录成功",'index/index/index');
 		}else{
-            $this->error("账号密码错误"， 'index/user/login');
-        }
+			$this->error("账号密码错误", 'index/user/login');
+		}
+
+	}
+
+
+	// 注册页
+	public function register(){
+		return $this->fetch();
+	}
+
+	// 提交注册
+	public function doRegister(){
+		$uname = input('post.uname');
+		$password = input('post.password');
+
+
+		$userM = new \app\index\model\User;
+		if($userM->hasUser($uname)){
+			$this->error('用户已存在，请使用其他用户名');
+		}
+
+		$userM->uname = $uname;
+		$userM->password = md5($password);
+		$userM->save();
+
+		$uid = $userM->uid;
+		if($uid){
+			Session::set('uid', $uid);
+			Session::set('uname', $uname);
+			$this->success('注册成功','index/index/index');
+		}else{
+			$this->error('新增用户失败','index/user/register');
+		}
 
 	}
 
--- application/index/model/User.php
+++ application/index/model/User.php
@@ -6,15 +6,27 @@ use think\Model;
 class User extends Model{
 	protected $table = 'fz_user';
 
+	// 登陆验证
 	public function login($name,$pass){
 		$user = $this->where(['name'=>$name,'pass'=>md5($pass)])->find();
 		if($user) return $user;
 		return false;
 	}
 	
+	// 获取用户信息
 	public function getUser($uid){
 		$user = $this->where('uid',$uid)->find();
 		return $user;
+	}
+
+	// 检查用户是否存在
+	public function hasUser($uname){
+		$user = $this->where('uname',$uname)->find();
+		if($user){
+			return true;
+		}else{
+			return false;
+		}
 	}
 	
 	
